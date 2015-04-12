272f2c5c19c28e30c10e1479798fdda32667f7a7
thetanuj <thetanuj@hotmail.com>
2015-04-12 15:57:42 +0530

    Rango

--- rango/views.py
+++ rango/views.py
@@ -33,6 +33,7 @@ def category(request, category_name_slug):
 	
 	return render(request, 'rango/category.html', context_dict)
 
+@login_required
 def add_category(request):
 	if request.method == 'POST':
 		form = CategoryForm(request.POST)
